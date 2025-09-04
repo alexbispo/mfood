4b4f3599c137bfe98e59c1ded647fa6363a5263c
Alex Bispo de Oliveira <alexbispo.artes@gmail.com>
2025-09-03 21:57:25 -0300

    [cadastro] - Adiciona testes para os recursos de Restaurante.

--- cadastro/src/test/java/com/github/alexbispo/mfood/cadastro/RestauranteResourceTest.java
+++ cadastro/src/test/java/com/github/alexbispo/mfood/cadastro/RestauranteResourceTest.java
@@ -4,10 +4,16 @@ import com.github.database.rider.cdi.api.DBRider;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.dataset.DataSet;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Parameters;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.approvaltests.Approvals;
 import org.approvaltests.core.Options;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -27,5 +33,28 @@ public class RestauranteResourceTest {
                 .then().statusCode(200).extract().body().asString();
 
         Approvals.verify(resposta, new Options().forFile().withExtension(".json"));
+    }
+
+    @Test
+    public void testCriarRestaurante() {
+        Restaurante novoRstaurante = new Restaurante();
+        novoRstaurante.nome = "Novo Restaurante";
+        novoRstaurante.proprietario = "ID do keyclock";
+        novoRstaurante.cnpj = "12345678901";
+
+        given()
+                .body(novoRstaurante)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/restaurantes")
+                .then().statusCode(201);
+
+        var restauranteCriado = Restaurante
+                .find("nome = :nome and proprietario = :proprietario and cnpj = :cnpj",
+                    Parameters.with("nome", novoRstaurante.nome)
+                            .and("proprietario", novoRstaurante.proprietario)
+                            .and("cnpj", novoRstaurante.cnpj)).firstResult();
+
+        Assertions.assertNotNull(restauranteCriado);
     }
 }
